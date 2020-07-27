f74e70f61f63fcc4c5387cdc2f938306bfec2b3b
TanazzahRehman <68633062+TanazzahRehman@users.noreply.github.com>
2020-07-27 19:37:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import Hello from './Hello';
 
 function App({full_name, age}) {
-  return
-  <div> 
-  Hello from App.js updated by {full_name} of age Age = {age - 7}
-  <br/>
-  <Hello nam = {full_name} ></Hello>
-  </div>
+  return <div> 
+   Hello from App.js updated by {full_name} of age Age = {age - 7}
+   <br/>
+   <Hello nam = {full_name} ></Hello>
+   </div>
 }
 
 export default App;
